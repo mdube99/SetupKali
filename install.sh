192bddfa6cbdcc2442836a612a5c92f73e713681
Mark Dube <mjdube99@gmail.com>
2022-11-21 17:26:55 -0600

    update

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ setup_custom_scripts() {
 
 install_apt_applications() {
   echo -e "\n Installing Apt Applications"
-  for $l in $(cat $APT_PACKAGES); do
+  for l in $(cat $APT_PACKAGES); do
     echo "$l"
   done
 }
