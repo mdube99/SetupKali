b286a4874ab7dee523f3930fc47201ea9b13babe
Mark Dube <mjdube99@gmail.com>
2022-11-24 21:37:53 -0600

    added i3 install script --untested--

--- install.sh
+++ install.sh
@@ -16,6 +16,8 @@ setup_custom_scripts() {
   fi
   # Download scripts into development folder
   git clone https://github.com/mdube99/scripts.git ~/development/scripts
+  # Restart zsh
+  exec zsh
 }
 
 install_apt_applications() {
@@ -39,12 +41,28 @@ install_go_applications() {
 
 install_opt_programs() {
   sudo mkdir /opt/namemash
-  curl "https://gist.githubusercontent.com/superkojiman/11076951/raw/74f3de7740acb197ecfa8340d07d3926a95e5d46/namemash.py" >> /opt/namemash/namemash.py
+  sudo wget -O /opt/namemash/namemash.py "https://gist.githubusercontent.com/superkojiman/11076951/raw/74f3de7740acb197ecfa8340d07d3926a95e5d46/namemash.py"
   sudo mkdir /opt/procdump
-  wget "https://download.sysinternals.com/files/Procdump.zip" /opt/procdump
+  sudo wget -O /opt/procdump/Procdump.zip "https://download.sysinternals.com/files/Procdump.zip"
+  sudo unzip /opt/procdump/Procdump.zip
+}
+
+setup_i3() {
+  sudo apt install -y i3
+  sudo cp ./plasma-i3.desktop /usr/share/xsessions/.
+  sudo cp ./i3-config $HOME/.config/i3/config
+  hotkeysRC="$XDG_CONFIG_HOME/kglobalshortcutsrc"
+  # Remove application launching shortcuts.
+  sed -i 's/_launch=[^,]*/_launch=none/g' $hotkeysRC
+  # Remove other global shortcuts.
+  sed -i 's/^\([^_].*\)=[^,]*/\1=none/g' $hotkeysRC
+  # Reload hotkeys.
+  kquitapp5 kglobalaccel && sleep 2s && kglobalaccel5 &  
+  kwriteconfig5 --file startkderc --group General --key systemdBoot false
 }
 
 setup_custom_scripts
 install_apt_applications
 install_python_applications
 install_opt_programs
+setup_i3
